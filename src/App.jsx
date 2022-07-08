be69afac6b5d7a9e07f96f61e33703fb23450a22
oben-desmond <obend678@gmail.com>
2022-07-08 23:52:34 +0100

    modularizing item

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 // graphqlclient, gql
 import { GraphQLClient, gql } from 'graphql-request';
+import TransactionItem from './components/TransactionItem';
 
 const graphcms = new GraphQLClient('https://api-us-west-2.graphcms.com/v2/cl5cv0ond0eo601tbcnyw43ip/master');
 
@@ -187,31 +188,9 @@ function App() {
 
         {transactions.map((transaction, index) => {
 
-          const trans_x1 = transaction;
-          let equal = false;
-          if (index > 0) {
-            const trans_x2 = transactions[index - 1]
-            // days ago of trans_x1 the same as trans_x2
-            equal = getDaysAgo(trans_x1.date) == getDaysAgo(trans_x2.date)
-
-          }
-
-          return (
-            <div key={index}>
-
-              {(index == 0 || !equal) && <h6><br /><br />{new Date(transaction.date).toDateString()}</h6>}
-              <div className={'bg-light'} key={index}>
-                <div className="card-body">
-                  <div className="row">
-                    <div className="col">{transaction.transaction_id}</div>
-                    <div className="col"> {transaction.type}</div>
-                    <div className="col">{transaction.transaction_status}</div>
-                    <div className="col">{transaction.amount}</div>
-                    <div className="col"><small>{getTimeAgo(transaction.date)}</small></div>
-                  </div>
-                </div>
-              </div>
-            </div>)
+             return(
+              <TransactionItem transaction={transaction} key={index} />
+             )
         }
         )}
 
@@ -230,59 +209,4 @@ const dateOptions = [6, 14, 21, 30, 60, 180, 365];
 const statusOptions = ["progress", "success", "failed"];
 const typeOptions = ["withdraw", "send", "deposit", "receive"];
 
-// convert days to days if less than 7 days or months, years if more than 7 days
-function convertDays(days) {
-  if (days < 7) {
-    return "less than 1 week";
-  } else if (days < 30) {
-    return Math.floor(days / 7) + " weeks";
-  } else if (days < 365) {
-    return Math.floor(days / 30) + " months";
-  } else {
-    return "1 year or more";
-  }
-}
 
-
-// timeago seconds, minutes, hours, weeks, months, years from Date.now()
-function getTimeAgo(time) {
-  const date = new Date(time);
-  const now = new Date();
-  const seconds = Math.floor((now - date) / 1000);
-  const minutes = Math.floor(seconds / 60);
-  const hours = Math.floor(minutes / 60);
-  const days = Math.floor(hours / 24);
-  const weeks = Math.floor(days / 7);
-  const months = Math.floor(weeks / 4);
-  const years = Math.floor(months / 12);
-  if (seconds < 60) {
-    return "just now";
-  }
-  if (minutes < 60) {
-    return minutes + " minutes ago";
-  }
-  if (hours < 24) {
-    return hours + " hours ago";
-  }
-  if (days < 7) {
-    return days + " days ago";
-  }
-  if (weeks < 4) {
-    return weeks + " weeks ago";
-  }
-  if (months < 12) {
-    return months + " months ago";
-  }
-  return years + " years ago";
-}
-
-
-// number of days from Date.now()
-function getDaysAgo(date) {
-  const now = new Date();
-  const seconds = Math.floor((now - date) / 1000);
-  const minutes = Math.floor(seconds / 60);
-  const hours = Math.floor(minutes / 60);
-  const days = Math.floor(hours / 24);
-  return days;
-}
---
+++ src/components/TransactionItem.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { getDaysAgo, getTimeAgo } from './utilities';
+
+const TransactionItem = ({transaction}) => {
+    const trans_x1 = transaction;
+    let equal = false;
+    if (index > 0) {
+      const trans_x2 = transactions[index - 1]
+      // days ago of trans_x1 the same as trans_x2
+      equal = getDaysAgo(trans_x1.date) == getDaysAgo(trans_x2.date)
+
+    }
+
+    return (
+      <div >
+
+        {(index == 0 || !equal) && <h6><br /><br />{new Date(transaction.date).toDateString()}</h6>}
+        <div className={'bg-light'} >
+          <div className="card-body">
+            <div className="row">
+              <div className="col">{transaction.transaction_id}</div>
+              <div className="col"> {transaction.type}</div>
+              <div className="col">{transaction.transaction_status}</div>
+              <div className="col">{transaction.amount}</div>
+              <div className="col"><small>{getTimeAgo(transaction.date)}</small></div>
+            </div>
+          </div>
+        </div>
+      </div>)
+}
+
+export default TransactionItem
